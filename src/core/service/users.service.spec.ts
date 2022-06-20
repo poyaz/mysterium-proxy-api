a5537cf5f10bb2267322237a8c2b178d037f624e
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-06-20 15:29:26 +0430

    Add TDD unit test for userService.creatte

--- src/core/service/users.service.spec.ts
+++ src/core/service/users.service.spec.ts
@@ -1,18 +1,89 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { UsersService } from './users.service';
+import {Test, TestingModule} from '@nestjs/testing';
+import {UsersService} from './users.service';
+import {mock, MockProxy} from 'jest-mock-extended';
+import {IGenericRepositoryInterface} from '../interface/i-generic-repository.interface';
+import {IIdentifier} from '../interface/i-identifier.interface';
+import {UsersModel} from '../model/users.model';
+import {InterfaceRepositoryEnum} from '../enum/interface-repository.enum';
+import {UnknownException} from '../exception/unknown.exception';
+import {UserRoleEnum} from '../enum/user-role.enum';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let usersRepository: MockProxy<IGenericRepositoryInterface<UsersModel>>;
+  let identifierMock: MockProxy<IIdentifier>;
 
   beforeEach(async () => {
+    usersRepository = mock<IGenericRepositoryInterface<UsersModel>>();
+
+    identifierMock = mock<IIdentifier>();
+    identifierMock.generateId.mockReturnValue('00000000-0000-0000-0000-000000000000');
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: InterfaceRepositoryEnum.USER_PG_REPOSITORY,
+          useValue: usersRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
+  it(`Should be defined`, () => {
     expect(service).toBeDefined();
+  });
+
+  describe(`Create user`, () => {
+    let inputCreateUserModel: UsersModel;
+    let outputCreateUserModel: UsersModel;
+
+    beforeEach(() => {
+      inputCreateUserModel = new UsersModel({
+        id: '',
+        username: 'my-user',
+        password: 'my-password',
+        role: UserRoleEnum.USER,
+        isEnable: true,
+        insertDate: new Date(),
+      });
+
+      outputCreateUserModel = new UsersModel({
+        id: identifierMock.generateId(),
+        username: 'my-user',
+        password: 'my-password',
+        role: UserRoleEnum.USER,
+        isEnable: true,
+        insertDate: new Date(),
+      });
+    });
+
+    it(`Should error create user`, async () => {
+      usersRepository.add.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.create(inputCreateUserModel);
+
+      expect(usersRepository.add).toHaveBeenCalled();
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should successfully create user`, async () => {
+      usersRepository.add.mockResolvedValue([null, outputCreateUserModel]);
+
+      const [error, result] = await service.create(inputCreateUserModel);
+
+      expect(usersRepository.add).toHaveBeenCalled();
+      expect(error).toBeNull();
+      expect(result).toMatchObject<UsersModel>({
+        id: outputCreateUserModel.id,
+        username: outputCreateUserModel.username,
+        password: outputCreateUserModel.password,
+        isEnable: outputCreateUserModel.isEnable,
+        role: outputCreateUserModel.role,
+        insertDate: outputCreateUserModel.insertDate,
+      });
+    });
   });
 });
--- src/core/service/users.service.ts
+++ src/core/service/users.service.ts
@@ -1,27 +1,37 @@
-import {Injectable} from '@nestjs/common';
+import {Inject, Injectable} from '@nestjs/common';
 import {IUsersService} from '../interface/i-users-service.interface';
 import {UsersModel} from '../model/users.model';
 import {UpdateModel} from '../model/update.model';
+import {AsyncReturn} from '../utility';
+import {FilterModel} from '../model/filter.model';
+import {IGenericRepositoryInterface} from '../interface/i-generic-repository.interface';
+import {InterfaceRepositoryEnum} from '../enum/interface-repository.enum';
 
 @Injectable()
 export class UsersService implements IUsersService {
-  async findAll() {
-    return [];
+  constructor(
+    @Inject(InterfaceRepositoryEnum.USER_PG_REPOSITORY)
+    private readonly _userRepository: IGenericRepositoryInterface<UsersModel>,
+  ) {
   }
 
-  async findOne(id: string) {
-    return [];
+  async create(model: UsersModel): Promise<AsyncReturn<Error, UsersModel>> {
+    return this._userRepository.add(model);
   }
 
-  async create(model: UsersModel) {
-    return [];
+  findAll(filter?: FilterModel): Promise<AsyncReturn<Error, Array<UsersModel>>> {
+    return Promise.resolve(undefined);
   }
 
-  async update(model: UpdateModel<UsersModel>) {
-    return [];
+  findOne(id: string): Promise<AsyncReturn<Error, UsersModel>> {
+    return Promise.resolve(undefined);
   }
 
-  async remove(id: string) {
-    return [];
+  update(model: UpdateModel<UsersModel>): Promise<AsyncReturn<Error, null>> {
+    return Promise.resolve(undefined);
+  }
+
+  remove(id: string): Promise<AsyncReturn<Error, null>> {
+    return Promise.resolve(undefined);
   }
 }
