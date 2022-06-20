b5ef9e0e70109ae64648b158835e9c6a8534c338
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-06-20 15:01:31 +0430

    Change all type of function to AsyncReturn

--- src/core/interface/i-users-service.interface.ts
+++ src/core/interface/i-users-service.interface.ts
@@ -1,19 +1,20 @@
 import {UsersModel} from '../model/users.model';
 import {FilterModel} from '../model/filter.model';
 import {UpdateModel} from '../model/update.model';
+import {AsyncReturn} from '../utility';
 
 export enum I_USER_SERVICE {
   DEFAULT = 'USERS_SERVICE_DEFAULT',
 }
 
 export interface IUsersService {
-  findAll(filter?: FilterModel): Promise<(Error | Array<UsersModel>)[]>;
+  findAll(filter?: FilterModel): Promise<AsyncReturn<Error, Array<UsersModel>>>;
 
-  findOne(id: string): Promise<(Error | UsersModel)[]>;
+  findOne(id: string): Promise<AsyncReturn<Error, UsersModel>>;
 
-  create(model: UsersModel): Promise<(Error | UsersModel)[]>;
+  create(model: UsersModel): Promise<AsyncReturn<Error, UsersModel>>;
 
-  update(model: UpdateModel<UsersModel>): Promise<(Error)[]>;
+  update(model: UpdateModel<UsersModel>): Promise<AsyncReturn<Error, null>>;
 
-  remove(id: string): Promise<(Error)[]>;
+  remove(id: string): Promise<AsyncReturn<Error, null>>;
 }
--- src/core/utility.d.ts
+++ src/core/utility.d.ts
@@ -1,3 +1,6 @@
+export type Maybe<T> = T | null;
+export type AsyncReturn<E, R> = [Maybe<E>, Maybe<R>?];
+
 export type ModelRequireProp<T> = Pick<T, { [K in keyof T]: T[K] extends Function ? never : K }[keyof T]>;
 
 export type UpdateInstanceType<T> = Partial<Omit<Pick<T, { [K in keyof T]: T[K] extends Function ? never : K }[keyof T]>, 'id' | 'insertDate'>>;
