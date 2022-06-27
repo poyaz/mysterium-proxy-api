05e6ef35cddf343e1770785d4450c3793de89876
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-06-27 14:49:28 +0430

    Add error exist in repository error

--- src/core/exception/repository.exception.ts
+++ src/core/exception/repository.exception.ts
@@ -1,10 +1,22 @@
 import {ExceptionEnum} from '../enum/exception.enum';
+import {UsersEntity} from '../../infrastructure/entity/users.entity';
+import {ExistException} from './exist.exception';
 
 export class RepositoryException extends Error {
   readonly isOperation: boolean;
   readonly additionalInfo: Error;
 
   constructor(error) {
+    if ('code' in error && error.code === '23505') {
+      const existError = new ExistException();
+      super(existError.message);
+
+      this.name = existError.name;
+      this.isOperation = existError.isOperation;
+
+      return;
+    }
+
     super('Repository error!');
 
     this.name = ExceptionEnum.REPOSITORY_ERROR;
