e6af097929f4fb3c57990ad76584262c39cfbc0f
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-07-17 09:01:01 +0430

    Change role name gaurd

---
+++ src/api/http/guard/role.guard.ts
@@ -0,0 +1,34 @@
+import {Injectable, CanActivate, ExecutionContext} from '@nestjs/common';
+import {Reflector} from '@nestjs/core';
+import {UserRoleEnum} from '@src-core/enum/user-role.enum';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const disableCheckAuth = this.reflector.get<string[]>('disableCheckAuth', context.getHandler());
+    if (disableCheckAuth) {
+      return true;
+    }
+
+    const roles = this.reflector.get<UserRoleEnum[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    if (roles.indexOf(request.user.role) === -1) {
+      return false;
+    }
+    if (request.user.role === UserRoleEnum.ADMIN) {
+      return true;
+    }
+    if (request.user.role === UserRoleEnum.USER && request.params['userId'] === request.user.userId) {
+      return true;
+    }
+
+    return false;
+  }
+}
