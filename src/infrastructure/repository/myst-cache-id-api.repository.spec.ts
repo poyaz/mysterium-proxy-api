6b6f235dd3eb886f2a6229af2192795f2918ea20
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-07-30 19:10:29 +0430

    Add TDD unit test for MystCacheIdApiRepository.getById

--- src/infrastructure/repository/myst-cache-id-api.repository.spec.ts
+++ src/infrastructure/repository/myst-cache-id-api.repository.spec.ts
@@ -1,5 +1,4 @@
 import {Test, TestingModule} from '@nestjs/testing';
-import axios from 'axios';
 import {MystCacheIdApiRepository} from './myst-cache-id-api.repository';
 import {mock, MockProxy} from 'jest-mock-extended';
 import {Redis} from 'ioredis';
@@ -15,14 +14,16 @@ import {
   VpnServiceTypeEnum,
 } from '@src-core/model/vpn-provider.model';
 import {RepositoryException} from '@src-core/exception/repository.exception';
-
-jest.mock('axios');
+import {Logger} from '@nestjs/common';
+import {FilterModel} from '@src-core/model/filter.model';
+import {MystIdentityModel} from '@src-core/model/myst-identity.model';
 
 describe('MystCacheIdApiRepository', () => {
   let repository: MystCacheIdApiRepository;
   let redis: MockProxy<Redis>;
   let redisService: MockProxy<RedisService>;
   let mystApiRepository: MockProxy<IProxyApiRepositoryInterface>;
+  let logger: MockProxy<Logger>;
   let identifierMock: MockProxy<IIdentifier>;
 
   beforeEach(async () => {
@@ -30,7 +31,7 @@ describe('MystCacheIdApiRepository', () => {
     redisService = mock<RedisService>();
     redisService.getClient.mockReturnValue(redis);
     mystApiRepository = mock<IProxyApiRepositoryInterface>();
-    redis.mset;
+    logger = mock<Logger>();
 
     identifierMock = mock<IIdentifier>();
     identifierMock.generateId.mockReturnValue('11111111-1111-1111-1111-111111111111');
@@ -46,10 +47,14 @@ describe('MystCacheIdApiRepository', () => {
           useValue: mystApiRepository,
         },
         {
+          provide: Logger,
+          useValue: logger,
+        },
+        {
           provide: MystCacheIdApiRepository,
-          inject: [RedisService, ProviderTokenEnum.MYST_CACHE_ID_API_REPOSITORY],
-          useFactory: (redisService: RedisService, mystApiRepository: IProxyApiRepositoryInterface) =>
-            new MystCacheIdApiRepository(redisService, mystApiRepository),
+          inject: [RedisService, ProviderTokenEnum.MYST_CACHE_ID_API_REPOSITORY, Logger],
+          useFactory: (redisService: RedisService, mystApiRepository: IProxyApiRepositoryInterface, logger: Logger) =>
+            new MystCacheIdApiRepository(redisService, mystApiRepository, logger),
         },
       ],
     }).compile();
@@ -111,7 +116,7 @@ describe('MystCacheIdApiRepository', () => {
         [outputVpnProviderData1, outputVpnProviderData2],
         2,
       ]);
-      const executeError = new Error('Error in create on database');
+      const executeError = new Error('Error in execute set on database');
       redis.mset.mockRejectedValue(executeError);
 
       const [error] = await repository.getAll();
@@ -150,6 +155,7 @@ describe('MystCacheIdApiRepository', () => {
       expect(redis.expire).toHaveBeenCalledTimes(2);
       expect(redis.expire.mock.calls[0]).toEqual(expect.arrayContaining([`myst_provider:${outputVpnProviderData1.id}`, 5 * 60]));
       expect(redis.expire.mock.calls[1]).toEqual(expect.arrayContaining([`myst_provider:${outputVpnProviderData2.id}`, 5 * 60]));
+      expect(logger.warn).toHaveBeenCalledTimes(0);
       expect(error).toBeNull();
       expect(result.length).toEqual(2);
       expect(result[0]).toEqual(<VpnProviderModel>{
@@ -173,6 +179,190 @@ describe('MystCacheIdApiRepository', () => {
         insertDate: outputVpnProviderData2.insertDate,
       });
       expect(totalCount).toEqual(2);
+    });
+
+    it(`Should successfully get all vpn provider (Async expire key has executed failure)`, async () => {
+      mystApiRepository.getAll.mockResolvedValue([
+        null,
+        [outputVpnProviderData1, outputVpnProviderData2],
+        2,
+      ]);
+      redis.mset.mockResolvedValue(null);
+      redis.expire
+        .mockResolvedValueOnce(null)
+        .mockRejectedValueOnce(new Error('Error in execute expire on database'));
+      logger.error.mockReturnValue(null);
+
+      const [error, result, totalCount] = await repository.getAll();
+      jest.advanceTimersToNextTimer(3);
+
+      expect(mystApiRepository.getAll).toHaveBeenCalled();
+      expect(redis.mset).toHaveBeenCalled();
+      expect(redis.mset).toBeCalledWith(expect.arrayContaining([
+        `myst_provider:${outputVpnProviderData1.id}`,
+        outputVpnProviderData1.providerIdentity,
+        `myst_provider:${outputVpnProviderData2.id}`,
+        outputVpnProviderData2.providerIdentity,
+      ]));
+      expect(redis.expire).toHaveBeenCalledTimes(2);
+      expect(redis.expire.mock.calls[0]).toEqual(expect.arrayContaining([`myst_provider:${outputVpnProviderData1.id}`, 5 * 60]));
+      expect(redis.expire.mock.calls[1]).toEqual(expect.arrayContaining([`myst_provider:${outputVpnProviderData2.id}`, 5 * 60]));
+      expect(error).toBeNull();
+      expect(result.length).toEqual(2);
+      expect(result[0]).toEqual(<VpnProviderModel>{
+        id: outputVpnProviderData1.id,
+        serviceType: outputVpnProviderData1.serviceType,
+        providerName: outputVpnProviderData1.providerName,
+        providerIdentity: outputVpnProviderData1.providerIdentity,
+        providerIpType: outputVpnProviderData1.providerIpType,
+        country: outputVpnProviderData1.country,
+        isRegister: outputVpnProviderData1.isRegister,
+        insertDate: outputVpnProviderData1.insertDate,
+      });
+      expect(result[1]).toEqual(<VpnProviderModel>{
+        id: outputVpnProviderData2.id,
+        serviceType: outputVpnProviderData2.serviceType,
+        providerName: outputVpnProviderData2.providerName,
+        providerIdentity: outputVpnProviderData2.providerIdentity,
+        providerIpType: outputVpnProviderData2.providerIpType,
+        country: outputVpnProviderData2.country,
+        isRegister: outputVpnProviderData2.isRegister,
+        insertDate: outputVpnProviderData2.insertDate,
+      });
+      expect(totalCount).toEqual(2);
+    });
+  });
+
+  describe(`Get vpn info by id`, () => {
+    let inputId;
+    let outputVpnProviderData: VpnProviderModel;
+    let matchFilterProviderIdentity: FilterModel<VpnProviderModel>;
+
+    beforeEach(() => {
+      inputId = identifierMock.generateId();
+
+      outputVpnProviderData = new VpnProviderModel({
+        id: '11111111-1111-1111-1111-111111111111',
+        serviceType: VpnServiceTypeEnum.WIREGUARD,
+        providerName: VpnProviderName.MYSTERIUM,
+        providerIdentity: 'providerIdentity1',
+        providerIpType: VpnProviderIpTypeEnum.HOSTING,
+        country: 'GB',
+        isRegister: false,
+        insertDate: new Date(),
+      });
+
+      matchFilterProviderIdentity = new FilterModel<VpnProviderModel>();
+      matchFilterProviderIdentity.addCondition({$opr: 'eq', providerIdentity: outputVpnProviderData.providerIdentity});
+    });
+
+    it(`Should error get vpn info by id when fetch data from getById (Fail read data from redis)`, async () => {
+      const executeError = new Error('Error in execute get on database');
+      redis.get.mockRejectedValue(executeError);
+      logger.error.mockReturnValue(null);
+      mystApiRepository.getById.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await repository.getById(inputId);
+
+      expect(redis.get).toHaveBeenCalled();
+      expect(logger.error).toHaveBeenCalled();
+      expect(mystApiRepository.getById).toHaveBeenCalled();
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should error get vpn info by id when fetch data from getById (Return null from redis)`, async () => {
+      redis.get.mockResolvedValue(null);
+      logger.error.mockReturnValue(null);
+      mystApiRepository.getById.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await repository.getById(inputId);
+
+      expect(redis.get).toHaveBeenCalled();
+      expect(logger.error).toBeCalledTimes(0);
+      expect(mystApiRepository.getById).toHaveBeenCalled();
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should successfully get vpn info by id when fetch data from getById (Return null from redis)`, async () => {
+      redis.get.mockResolvedValue(null);
+      logger.error.mockReturnValue(null);
+      mystApiRepository.getById.mockResolvedValue([null, outputVpnProviderData]);
+
+      const [error, result] = await repository.getById(inputId);
+
+      expect(redis.get).toHaveBeenCalled();
+      expect(logger.error).toBeCalledTimes(0);
+      expect(mystApiRepository.getById).toHaveBeenCalled();
+      expect(error).toBeNull();
+      expect(result).toEqual(<VpnProviderModel>{
+        id: outputVpnProviderData.id,
+        serviceType: outputVpnProviderData.serviceType,
+        providerName: outputVpnProviderData.providerName,
+        providerIdentity: outputVpnProviderData.providerIdentity,
+        providerIpType: outputVpnProviderData.providerIpType,
+        country: outputVpnProviderData.country,
+        isRegister: outputVpnProviderData.isRegister,
+        insertDate: outputVpnProviderData.insertDate,
+      });
+    });
+
+    it(`Should error get vpn info by id when fetch data from getAll with filter (Return data from redis)`, async () => {
+      redis.get.mockResolvedValue(outputVpnProviderData.providerIdentity);
+      mystApiRepository.getAll.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await repository.getById(inputId);
+
+      expect(redis.get).toHaveBeenCalled();
+      expect(logger.error).toBeCalledTimes(0);
+      expect(mystApiRepository.getAll).toHaveBeenCalled();
+      expect((<FilterModel<VpnProviderModel>>mystApiRepository.getAll.mock.calls[0][0]).getCondition('providerIdentity')).toMatchObject({
+        $opr: 'eq',
+        providerIdentity: outputVpnProviderData.providerIdentity,
+      });
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should successfully get vpn info by id when fetch data from getAll with filter and return null when not found (Return data from redis)`, async () => {
+      redis.get.mockResolvedValue(outputVpnProviderData.providerIdentity);
+      mystApiRepository.getAll.mockResolvedValue([null, []]);
+
+      const [error, result] = await repository.getById(inputId);
+
+      expect(redis.get).toHaveBeenCalled();
+      expect(logger.error).toBeCalledTimes(0);
+      expect(mystApiRepository.getAll).toHaveBeenCalled();
+      expect((<FilterModel<VpnProviderModel>>mystApiRepository.getAll.mock.calls[0][0]).getCondition('providerIdentity')).toMatchObject({
+        $opr: 'eq',
+        providerIdentity: outputVpnProviderData.providerIdentity,
+      });
+      expect(error).toBeNull();
+      expect(result).toBeNull();
+    });
+
+    it(`Should successfully get vpn info by id when fetch data from getAll with filter (Return data from redis)`, async () => {
+      redis.get.mockResolvedValue(outputVpnProviderData.providerIdentity);
+      mystApiRepository.getAll.mockResolvedValue([null, [outputVpnProviderData]]);
+
+      const [error, result] = await repository.getById(inputId);
+
+      expect(redis.get).toHaveBeenCalled();
+      expect(logger.error).toBeCalledTimes(0);
+      expect(mystApiRepository.getAll).toHaveBeenCalled();
+      expect((<FilterModel<VpnProviderModel>>mystApiRepository.getAll.mock.calls[0][0]).getCondition('providerIdentity')).toMatchObject({
+        $opr: 'eq',
+        providerIdentity: outputVpnProviderData.providerIdentity,
+      });
+      expect(error).toBeNull();
+      expect(result).toEqual(<VpnProviderModel>{
+        id: outputVpnProviderData.id,
+        serviceType: outputVpnProviderData.serviceType,
+        providerName: outputVpnProviderData.providerName,
+        providerIdentity: outputVpnProviderData.providerIdentity,
+        providerIpType: outputVpnProviderData.providerIpType,
+        country: outputVpnProviderData.country,
+        isRegister: outputVpnProviderData.isRegister,
+        insertDate: outputVpnProviderData.insertDate,
+      });
     });
   });
 });
--- src/infrastructure/repository/myst-cache-id-api.repository.ts
+++ src/infrastructure/repository/myst-cache-id-api.repository.ts
@@ -1,10 +1,11 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, Logger} from '@nestjs/common';
 import {IProxyApiRepositoryInterface} from '@src-core/interface/i-proxy-api-repository.interface';
 import Redis from 'ioredis';
 import {RedisService} from '@liaoliaots/nestjs-redis';
 import {AsyncReturn} from '@src-core/utility';
 import {VpnProviderModel} from '@src-core/model/vpn-provider.model';
 import {RepositoryException} from '@src-core/exception/repository.exception';
+import {FilterModel} from '@src-core/model/filter.model';
 
 @Injectable()
 export class MystCacheIdApiRepository implements IProxyApiRepositoryInterface {
@@ -15,6 +16,7 @@ export class MystCacheIdApiRepository implements IProxyApiRepositoryInterface {
   constructor(
     private readonly _redisService: RedisService,
     private readonly _mystApiRepository: IProxyApiRepositoryInterface,
+    private readonly _logger: Logger,
   ) {
     this._redis = this._redisService.getClient();
   }
@@ -34,7 +36,13 @@ export class MystCacheIdApiRepository implements IProxyApiRepositoryInterface {
       for (let i = 0; i < addCacheData.length; i = i + 2) {
         tasks.push(this._redis.expire(addCacheData[i], MystCacheIdApiRepository.EXPIRE_KEY));
       }
-      Promise.all(tasks).catch(() => ({}));
+      Promise.all(tasks).catch((error) => {
+        this._logger.error(
+          `Fail to update expire key for prefix "${MystCacheIdApiRepository.PREFIX_KEY}"`,
+          error.stack,
+          this.constructor.name,
+        );
+      });
 
       return [null, vpnDataList, totalCount];
     } catch (error) {
@@ -42,7 +50,38 @@ export class MystCacheIdApiRepository implements IProxyApiRepositoryInterface {
     }
   }
 
-  getById(id: string): Promise<AsyncReturn<Error, VpnProviderModel | null>> {
-    return Promise.resolve(undefined);
+  async getById(id: string): Promise<AsyncReturn<Error, VpnProviderModel | null>> {
+    const providerIdentity = await this._getProviderIdCache(id);
+    if (!providerIdentity) {
+      return this._mystApiRepository.getById(id);
+    }
+
+    const filter = new FilterModel<VpnProviderModel>();
+    filter.addCondition({$opr: 'eq', providerIdentity});
+
+    const [error, dataList] = await this._mystApiRepository.getAll(filter);
+    if (error) {
+      return [error];
+    }
+
+    if (dataList.length === 0) {
+      return [null, null];
+    }
+
+    return [null, dataList[0]];
+  }
+
+  private async _getProviderIdCache(id: string): Promise<string | null> {
+    try {
+      return await this._redis.get(`${MystCacheIdApiRepository.PREFIX_KEY}:${id}`);
+    } catch (error) {
+      this._logger.error(
+        `Fail to read key for prefix "${MystCacheIdApiRepository.PREFIX_KEY}"`,
+        error.stack,
+        this.constructor.name,
+      );
+
+      return null;
+    }
   }
 }
