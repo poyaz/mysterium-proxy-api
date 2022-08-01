500cf26063b75d829d4394aa71c40766ecf05748
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-08-01 15:53:57 +0430

    Rempove runnerModel declaration

--- src/core/interface/i-proxy-api-repository.interface.ts
+++ src/core/interface/i-proxy-api-repository.interface.ts
@@ -1,6 +1,5 @@
 import {AsyncReturn} from '@src-core/utility';
 import {VpnProviderModel} from '@src-core/model/vpn-provider.model';
-import {RunnerModel} from '@src-core/model/runner.model';
 
 export interface IProxyApiRepositoryInterface {
   getAll<F>(filter?: F): Promise<AsyncReturn<Error, Array<VpnProviderModel>>>;
