5b83507061770f6af6472831286931e24c947b8e
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-10-22 15:32:44 +0330

    Rename to docker-ruuner-create-myst

---
+++ src/infrastructure/repository/docker-runner-create-strategy.repository.spec.ts
@@ -0,0 +1,7 @@
+import { DockerRunnerCreateStrategyRepository } from './docker-runner-create-strategy.repository';
+
+describe('DockerRunnerCreateStrategyRepository', () => {
+  it('should be defined', () => {
+    expect(new DockerRunnerCreateStrategyRepository()).toBeDefined();
+  });
+});
