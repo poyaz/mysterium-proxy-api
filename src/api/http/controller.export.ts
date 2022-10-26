46176bbb9024fb67a9c05b93ba7ae96c3123b9f6
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-10-26 13:29:53 +0330

    Add MystProviderHttpController on controller list

--- src/api/http/controller.export.ts
+++ src/api/http/controller.export.ts
@@ -1,7 +1,9 @@
 import {UsersHttpController} from '@src-api/http/controller/users/users.http.controller';
 import {IdentityHttpController} from '@src-api/http/controller/identity/identity.http.controller';
+import {MystProviderHttpController} from '@src-api/http/controller/provider/myst/myst-provider-http.controller';
 
 export const controllersExport = [
   UsersHttpController,
   IdentityHttpController,
+  MystProviderHttpController,
 ];
