5bec21090993d7b3dfac839435061cbee01e7516
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-10-26 16:18:39 +0330

    Change http output /home/project/self/mysterium-proxy-api/src/api/http/interceptor/output.transfer.interceptor.ts

--- src/api/http/controller/provider/myst/myst-provider-http.controller.ts
+++ src/api/http/controller/provider/myst/myst-provider-http.controller.ts
@@ -1,7 +1,12 @@
 import {
   Controller,
+  Delete,
   Get,
-  Inject,
+  HttpCode,
+  HttpStatus,
+  Inject, Param,
+  Post,
+  Put,
   Query,
   UseGuards,
   UseInterceptors,
@@ -10,12 +15,9 @@ import {IProviderServiceInterface} from '@src-core/interface/i-provider-service.
 import {ProviderTokenEnum} from '@src-core/enum/provider-token.enum';
 import {
   ApiBadRequestResponse,
-  ApiBearerAuth,
-  ApiExtraModels,
-  ApiForbiddenResponse,
+  ApiBearerAuth, ApiExtraModels, ApiForbiddenResponse, ApiNoContentResponse, ApiNotFoundResponse,
   ApiOkResponse,
-  ApiOperation,
-  ApiQuery,
+  ApiOperation, ApiParam, ApiQuery,
   ApiTags,
   ApiUnauthorizedResponse,
   getSchemaPath,
@@ -30,14 +32,17 @@ import {ForbiddenExceptionDto} from '@src-api/http/dto/forbidden-exception.dto';
 import {FindProviderOutputDto} from '@src-api/http/controller/provider/dto/find-provider-output.dto';
 import {RemoveSpecialFieldOfProviderInterceptor} from '@src-api/http/controller/provider/interceptor/remove-special-field-of-provider.interceptor';
 import {FindProviderQueryDto} from '@src-api/http/controller/provider/dto/find-provider-query.dto';
+import {NotFoundExceptionDto} from '@src-api/http/dto/not-found-exception.dto';
+import {ExceptionEnum} from '@src-core/enum/exception.enum';
+import {NoBodySuccessDto} from '@src-api/http/dto/no-body-success.dto';
 
 @Controller({
   path: 'provider/myst',
   version: '1',
 })
 @UseGuards(RoleGuard)
-@Roles(UserRoleEnum.ADMIN)
 @UseInterceptors(RemoveSpecialFieldOfProviderInterceptor)
+@Roles(UserRoleEnum.ADMIN)
 @ApiTags('provider')
 @ApiBearerAuth()
 @ApiExtraModels(DefaultArraySuccessDto, FindProviderQueryDto, FindProviderOutputDto)
@@ -110,5 +115,57 @@ export class MystProviderHttpController {
   })
   async findAll(@Query() queryFilterDto: FindProviderQueryDto) {
     return this._providerService.getAll(FindProviderQueryDto.toModel(queryFilterDto));
+  }
+
+  @Put(':providerId')
+  @ApiOperation({description: 'Connect to myst vpn provider', operationId: 'Connect myst provider'})
+  @ApiParam({name: 'providerId', type: String, example: '00000000-0000-0000-0000-000000000000'})
+  @ApiOkResponse({
+    description: 'The myst vpn has been successfully connected.',
+    type: NoBodySuccessDto,
+  })
+  @ApiNotFoundResponse({
+    description: 'The provider id not found.',
+    schema: {
+      allOf: [
+        {$ref: getSchemaPath(NotFoundExceptionDto)},
+        {
+          properties: {
+            action: {
+              example: ExceptionEnum.NOT_FOUND_ERROR,
+            },
+          },
+        },
+      ],
+    },
+  })
+  async connect(@Param('providerId') providerId: string) {
+    return this._providerService.up(providerId);
+  }
+
+  @Delete(':providerId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({description: 'Disconnect form myst vpn provider', operationId: 'Disconnect myst provider'})
+  @ApiParam({name: 'providerId', type: String, example: '00000000-0000-0000-0000-000000000000'})
+  @ApiNoContentResponse({
+    description: 'The myst vpn has been successfully disconnected.',
+  })
+  @ApiNotFoundResponse({
+    description: 'The provider id not found.',
+    schema: {
+      allOf: [
+        {$ref: getSchemaPath(NotFoundExceptionDto)},
+        {
+          properties: {
+            action: {
+              example: ExceptionEnum.NOT_FOUND_ERROR,
+            },
+          },
+        },
+      ],
+    },
+  })
+  async disconnect(@Param('providerId') providerId: string) {
+
   }
 }
