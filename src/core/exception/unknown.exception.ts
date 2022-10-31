32d43dc469371b25076a1e218446407b64d7b9f1
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-10-31 16:58:14 +0330

    Add error param if need

--- src/core/exception/unknown.exception.ts
+++ src/core/exception/unknown.exception.ts
@@ -2,11 +2,16 @@ import {ExceptionEnum} from '@src-core/enum/exception.enum';
 
 export class UnknownException extends Error {
   readonly isOperation: boolean;
+  readonly additionalInfo?: Error;
 
-  constructor() {
+  constructor(error?: Error) {
     super('Unknown error happened!');
 
     this.name = ExceptionEnum.UNKNOWN_ERROR;
     this.isOperation = false;
+
+    if (error) {
+      this.additionalInfo = error;
+    }
   }
 }
