4a2cf7b3db3093a4601c4cc8dcecbfec4025b210
poyaz <pooya_azarpour@yahoo.com>
2022-11-04 14:36:13 +0330

    Add swagger doc for identity

--- src/api/http/controller/identity/identity.http.controller.ts
+++ src/api/http/controller/identity/identity.http.controller.ts
@@ -1,7 +1,7 @@
 import {
   Body,
   Controller, Delete,
-  Get, HttpCode, HttpStatus, Inject,
+  Get, HttpCode, HttpStatus, Inject, NestInterceptor,
   Param, Post,
   Query,
   UploadedFile,
@@ -26,7 +26,9 @@ import {DefaultExceptionDto} from '@src-api/http/dto/default-exception.dto';
 import {Roles} from '@src-api/http/decorator/roles.decorator';
 import {UserRoleEnum} from '@src-core/enum/user-role.enum';
 import {FindIdentityQueryDto} from '@src-api/http/controller/identity/dto/find-identity-query.dto';
-import {RemoveSpecialFieldOfIdentityInterceptor} from '@src-api/http/controller/identity/interceptor/remove-special-field-of-identity.interceptor';
+import {
+  RemoveSpecialFieldOfIdentityInterceptor
+} from '@src-api/http/controller/identity/interceptor/remove-special-field-of-identity.interceptor';
 import {ForbiddenExceptionDto} from '@src-api/http/dto/forbidden-exception.dto';
 import {FindIdentityOutputDto} from '@src-api/http/controller/identity/dto/find-identity-output.dto';
 import {ExceptionEnum} from '@src-core/enum/exception.enum';
@@ -34,30 +36,11 @@ import {NotFoundException} from '@src-core/exception/not-found.exception';
 import {FileInterceptor} from '@nestjs/platform-express';
 import {CreateIdentityInputDto} from '@src-api/http/controller/identity/dto/create-identity-input.dto';
 import {ValidateExceptionDto} from '@src-api/http/dto/validate-exception.dto';
-import {IdentityJsonFileValidationPipe} from '@src-api/http/controller/identity/pipe/identity-json-file-validation.pipe';
+import {
+  IdentityJsonFileValidationPipe
+} from '@src-api/http/controller/identity/pipe/identity-json-file-validation.pipe';
 import {ProviderTokenEnum} from '@src-core/enum/provider-token.enum';
 import {IMystIdentityServiceInterface} from '@src-core/interface/i-myst-identity-service.interface';
-
-const mystIdentityModelList = [
-  new MystIdentityModel({
-    id: 'e88ff52a-44c9-4592-9421-bda9ccfe4f57',
-    identity: '0x0e97730858ec6065541e99f08cf9d1809ddb7aa1',
-    passphrase: 'password',
-    path: '/path/of/identity/0x0e97730858ec6065541e99f08cf9d1809ddb7aa1',
-    filename: '0x0e97730858ec6065541e99f08cf9d1809ddb7aa1.json',
-    isUse: false,
-    insertDate: new Date(),
-  }),
-  new MystIdentityModel({
-    id: '90a0ec35-21c0-4b5d-af06-c78d1c12813b',
-    identity: '0x2bb00f2600989c06d2d552a003453c84bfa494ba',
-    passphrase: 'password',
-    path: '/path/of/identity/0x2bb00f2600989c06d2d552a003453c84bfa494ba',
-    filename: '0x2bb00f2600989c06d2d552a003453c84bfa494ba.json',
-    isUse: true,
-    insertDate: new Date(),
-  }),
-];
 
 @Controller({
   path: 'identity/myst/',
@@ -68,10 +51,17 @@ const mystIdentityModelList = [
 @Roles(UserRoleEnum.ADMIN)
 @ApiTags('identity')
 @ApiBearerAuth()
-@ApiExtraModels(DefaultSuccessDto, DefaultArraySuccessDto, NotFoundExceptionDto, FindIdentityQueryDto, FindIdentityOutputDto, CreateIdentityInputDto)
+@ApiExtraModels(
+  DefaultSuccessDto,
+  DefaultArraySuccessDto,
+  DefaultExceptionDto,
+  NotFoundExceptionDto,
+  FindIdentityQueryDto,
+  FindIdentityOutputDto,
+  CreateIdentityInputDto,
+)
 @ApiUnauthorizedResponse({description: 'Unauthorized', type: UnauthorizedExceptionDto})
 @ApiForbiddenResponse({description: 'Forbidden', type: ForbiddenExceptionDto})
-@ApiBadRequestResponse({description: 'Bad Request', type: DefaultExceptionDto})
 export class IdentityHttpController {
   constructor(
     @Inject(ProviderTokenEnum.MYST_IDENTITY_SERVICE_DEFAULT)
@@ -143,6 +133,7 @@ export class IdentityHttpController {
       ],
     },
   })
+  @ApiBadRequestResponse({description: 'Bad Request', type: DefaultExceptionDto})
   async findAll(@Query() queryFilterDto: FindIdentityQueryDto) {
     return this._mystIdentityService.getAll(FindIdentityQueryDto.toModel(queryFilterDto));
   }
@@ -151,6 +142,7 @@ export class IdentityHttpController {
   @ApiOperation({description: 'Get info of one identity with ID', operationId: 'Get identity'})
   @ApiParam({name: 'identityId', type: String, example: '00000000-0000-0000-0000-000000000000'})
   @ApiOkResponse({
+    description: 'Get identity data',
     schema: {
       allOf: [
         {$ref: getSchemaPath(DefaultSuccessDto)},
@@ -169,23 +161,79 @@ export class IdentityHttpController {
     description: 'The identity id not found.',
     schema: {
       allOf: [
-        {$ref: getSchemaPath(NotFoundExceptionDto)},
+        {
+          $ref: getSchemaPath(NotFoundExceptionDto)
+        },
         {
           properties: {
             action: {
-              example: ExceptionEnum.NOT_FOUND_ERROR,
+              example: ExceptionEnum.NOT_FOUND_MYST_IDENTITY_ERROR,
             },
           },
         },
       ],
-    },
+    }
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      anyOf: [
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.UNKNOWN_ERROR,
+              description: 'Unknown error happened',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+          ],
+        },
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.ADDRESS_IDENTITY,
+              description: 'Can not found identity address',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+            {
+              properties: {
+                action: {
+                  example: ExceptionEnum.ADDRESS_IDENTITY,
+                },
+              },
+            },
+          ],
+        },
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.PARSE_IDENTITY,
+              description: 'Can not parse identity file',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+            {
+              properties: {
+                action: {
+                  example: ExceptionEnum.PARSE_IDENTITY,
+                },
+              },
+            },
+          ],
+        },
+      ]
+    }
   })
   async findOne(@Param('identityId') identityId: string) {
     return this._mystIdentityService.getById(identityId);
   }
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(<NestInterceptor><any>FileInterceptor('file'))
   @ApiOperation({description: 'Add new identity', operationId: 'Add new identity'})
   @ApiConsumes('multipart/form-data')
   @ApiBody({
@@ -221,6 +269,79 @@ export class IdentityHttpController {
     },
   })
   @ApiUnprocessableEntityResponse({description: 'Unprocessable Entity', type: ValidateExceptionDto})
+  @ApiNotFoundResponse({description: 'The identity service not found', type: NotFoundException})
+  @ApiBadRequestResponse({
+    description: 'Bad request',
+    schema: {
+      anyOf: [
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.UNKNOWN_ERROR,
+              description: 'Unknown error happened',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+          ],
+        },
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.NOT_RUNNING_SERVICE,
+              description: 'Can not found running myst identity service',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+            {
+              properties: {
+                action: {
+                  example: ExceptionEnum.NOT_FOUND_MYST_IDENTITY_ERROR,
+                },
+              },
+            },
+          ],
+        },
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.EXIST_ERROR,
+              description: 'Item is exist',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+            {
+              properties: {
+                action: {
+                  example: ExceptionEnum.EXIST_ERROR,
+                },
+              },
+            },
+          ],
+        },
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.INVALID_FILE_TYPE,
+              description: 'Invalid identity file type',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+            {
+              properties: {
+                action: {
+                  example: ExceptionEnum.INVALID_FILE_TYPE,
+                },
+              },
+            },
+          ],
+        },
+      ],
+    },
+  })
   async create(
     @Body() createIdentityDto: CreateIdentityInputDto,
     @UploadedFile(IdentityJsonFileValidationPipe) file: Express.Multer.File,
@@ -239,13 +360,69 @@ export class IdentityHttpController {
     description: 'The identity id not found.',
     schema: {
       allOf: [
-        {$ref: getSchemaPath(NotFoundExceptionDto)},
+        {
+          $ref: getSchemaPath(NotFoundExceptionDto),
+        },
         {
           properties: {
             action: {
-              example: ExceptionEnum.NOT_FOUND_USER_ERROR,
+              example: ExceptionEnum.NOT_FOUND_MYST_IDENTITY_ERROR,
             },
           },
+        },
+      ],
+    },
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad request',
+    schema: {
+      anyOf: [
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.UNKNOWN_ERROR,
+              description: 'Unknown error happened',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+          ],
+        },
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.MYST_IDENTITY_IN_USE,
+              description: 'The identity is in use',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+            {
+              properties: {
+                action: {
+                  example: ExceptionEnum.MYST_IDENTITY_IN_USE,
+                },
+              },
+            },
+          ],
+        },
+        {
+          allOf: [
+            {
+              title: ExceptionEnum.INVALID_FILE_TYPE,
+              description: 'Invalid identity file type',
+            },
+            {
+              $ref: getSchemaPath(DefaultExceptionDto),
+            },
+            {
+              properties: {
+                action: {
+                  example: ExceptionEnum.INVALID_FILE_TYPE,
+                },
+              },
+            },
+          ],
         },
       ],
     },
