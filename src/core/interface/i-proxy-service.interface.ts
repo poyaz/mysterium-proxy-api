f5649bd9b79b1aa0cfc40f8f252f9cae90bf5370
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-11-07 15:28:58 +0330

    Add proxy service interface

---
+++ src/core/interface/i-proxy-service.interface.ts
@@ -0,0 +1,11 @@
+import {FilterModel} from '@src-core/model/filter.model';
+import {AsyncReturn} from '@src-core/utility';
+import {ProxyUpstreamModel} from '@src-core/model/proxy.model';
+
+export interface IProxyServiceInterface {
+  getAll(filter?: FilterModel<ProxyUpstreamModel>): Promise<AsyncReturn<Error, Array<ProxyUpstreamModel>>>;
+
+  create(model: ProxyUpstreamModel): Promise<AsyncReturn<Error, ProxyUpstreamModel>>;
+
+  remove(id: string): Promise<AsyncReturn<ErrorEvent, null>>;
+}
