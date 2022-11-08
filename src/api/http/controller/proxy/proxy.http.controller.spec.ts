b4750097a0ddf5d2249c7e69625cd03137355db3
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-11-08 11:42:12 +0330

    Change proxy model data for test

--- src/api/http/controller/proxy/proxy.http.controller.spec.ts
+++ src/api/http/controller/proxy/proxy.http.controller.spec.ts
@@ -58,7 +58,7 @@ describe('Proxy.HttpController', () => {
   describe(`Find all proxy`, () => {
     let inputFindProxyQueryDto: FindProxyQueryDto;
     let inputEmptyFindProxyQueryDto: FindProxyQueryDto;
-    let outputRunnerDownstreamModel: RunnerModel<MystIdentityModel>;
+    let outputRunnerDownstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
     let outputDownstreamProxyModel: ProxyDownstreamModel;
     let outputRunnerUpstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
     let outputProxyUpstreamModel: ProxyUpstreamModel;
@@ -69,27 +69,32 @@ describe('Proxy.HttpController', () => {
 
       inputEmptyFindProxyQueryDto = new FindProxyQueryDto();
 
-      outputRunnerDownstreamModel = new RunnerModel<MystIdentityModel>({
-        id: '11111111-1111-1111-1111-111111111111',
-        serial: 'myst-serial',
-        name: 'myst-name',
-        service: RunnerServiceEnum.MYST,
+      outputRunnerDownstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
+        id: '33333333-3333-3333-3333-333333333333',
+        serial: 'envoy-serial',
+        name: 'envoy-name',
+        service: RunnerServiceEnum.ENVOY,
         exec: RunnerExecEnum.DOCKER,
-        socketType: RunnerSocketTypeEnum.HTTP,
-        socketUri: '10.10.10.1',
-        socketPort: 4449,
+        socketType: RunnerSocketTypeEnum.TCP,
+        socketUri: '10.10.10.2',
+        socketPort: 10001,
         status: RunnerStatusEnum.RUNNING,
         insertDate: new Date(),
       });
-      outputRunnerDownstreamModel.label = {
-        $namespace: MystIdentityModel.name,
-        id: '22222222-2222-2222-2222-222222222222',
-        identity: 'identity1',
-      };
+      outputRunnerDownstreamModel.label = [
+        {
+          $namespace: MystIdentityModel.name,
+          id: '44444444-4444-4444-4444-444444444444',
+        },
+        {
+          $namespace: VpnProviderModel.name,
+          id: '55555555-5555-5555-5555-5555555555555',
+        },
+      ];
 
       outputDownstreamProxyModel = new ProxyDownstreamModel({
-        id: '33333333-3333-3333-3333-333333333333',
-        refId: '44444444-4444-4444-4444-444444444444',
+        id: '66666666-6666-6666-6666-666666666666',
+        refId: '77777777-7777-7777-7777-777777777777',
         ip: '25.14.65.1',
         mask: 32,
         type: ProxyTypeEnum.MYST,
@@ -98,13 +103,13 @@ describe('Proxy.HttpController', () => {
       });
 
       outputRunnerUpstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
-        id: '55555555-5555-5555-5555-555555555555',
+        id: '88888888-8888-8888-8888-888888888888',
         serial: 'socat-serial',
         name: 'socat-name',
         service: RunnerServiceEnum.SOCAT,
         exec: RunnerExecEnum.DOCKER,
         socketType: RunnerSocketTypeEnum.TCP,
-        socketUri: '10.10.10.2',
+        socketUri: '10.10.10.3',
         socketPort: 3128,
         status: RunnerStatusEnum.RUNNING,
         insertDate: new Date(),
@@ -112,11 +117,11 @@ describe('Proxy.HttpController', () => {
       outputRunnerUpstreamModel.label = [
         {
           $namespace: MystIdentityModel.name,
-          id: '22222222-2222-2222-2222-222222222222',
+          id: '44444444-4444-4444-4444-444444444444',
         },
         {
           $namespace: VpnProviderModel.name,
-          id: '66666666-6666-6666-6666-666666666666',
+          id: '55555555-5555-5555-5555-5555555555555',
         },
       ];
 
@@ -185,7 +190,7 @@ describe('Proxy.HttpController', () => {
   describe(`Create new proxy`, () => {
     let inputProxyWithoutPort: CreateProxyInputDto;
     let inputProxyWithPort: CreateProxyInputDto;
-    let outputRunnerDownstreamModel: RunnerModel<MystIdentityModel>;
+    let outputRunnerDownstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
     let outputDownstreamProxyModel: ProxyDownstreamModel;
     let outputRunnerUpstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
     let outputProxyUpstreamModel: ProxyUpstreamModel;
@@ -198,27 +203,32 @@ describe('Proxy.HttpController', () => {
       inputProxyWithPort.providerId = identifierMock.generateId();
       inputProxyWithPort.listenPort = 3128;
 
-      outputRunnerDownstreamModel = new RunnerModel<MystIdentityModel>({
-        id: '11111111-1111-1111-1111-111111111111',
-        serial: 'myst-serial',
-        name: 'myst-name',
-        service: RunnerServiceEnum.MYST,
+      outputRunnerDownstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
+        id: '33333333-3333-3333-3333-333333333333',
+        serial: 'envoy-serial',
+        name: 'envoy-name',
+        service: RunnerServiceEnum.ENVOY,
         exec: RunnerExecEnum.DOCKER,
-        socketType: RunnerSocketTypeEnum.HTTP,
-        socketUri: '10.10.10.1',
-        socketPort: 4449,
+        socketType: RunnerSocketTypeEnum.TCP,
+        socketUri: '10.10.10.2',
+        socketPort: 10001,
         status: RunnerStatusEnum.RUNNING,
         insertDate: new Date(),
       });
-      outputRunnerDownstreamModel.label = {
-        $namespace: MystIdentityModel.name,
-        id: '22222222-2222-2222-2222-222222222222',
-        identity: 'identity1',
-      };
+      outputRunnerDownstreamModel.label = [
+        {
+          $namespace: MystIdentityModel.name,
+          id: '44444444-4444-4444-4444-444444444444',
+        },
+        {
+          $namespace: VpnProviderModel.name,
+          id: '55555555-5555-5555-5555-5555555555555',
+        },
+      ];
 
       outputDownstreamProxyModel = new ProxyDownstreamModel({
-        id: '33333333-3333-3333-3333-333333333333',
-        refId: '44444444-4444-4444-4444-444444444444',
+        id: '66666666-6666-6666-6666-666666666666',
+        refId: '77777777-7777-7777-7777-777777777777',
         ip: '25.14.65.1',
         mask: 32,
         type: ProxyTypeEnum.MYST,
@@ -227,13 +237,13 @@ describe('Proxy.HttpController', () => {
       });
 
       outputRunnerUpstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
-        id: '55555555-5555-5555-5555-555555555555',
+        id: '88888888-8888-8888-8888-888888888888',
         serial: 'socat-serial',
         name: 'socat-name',
         service: RunnerServiceEnum.SOCAT,
         exec: RunnerExecEnum.DOCKER,
         socketType: RunnerSocketTypeEnum.TCP,
-        socketUri: '10.10.10.2',
+        socketUri: '10.10.10.3',
         socketPort: 3128,
         status: RunnerStatusEnum.RUNNING,
         insertDate: new Date(),
@@ -241,17 +251,17 @@ describe('Proxy.HttpController', () => {
       outputRunnerUpstreamModel.label = [
         {
           $namespace: MystIdentityModel.name,
-          id: '22222222-2222-2222-2222-222222222222',
+          id: '44444444-4444-4444-4444-444444444444',
         },
         {
           $namespace: VpnProviderModel.name,
-          id: '66666666-6666-6666-6666-666666666666',
+          id: '55555555-5555-5555-5555-5555555555555',
         },
       ];
 
       outputProxyUpstreamModel = new ProxyUpstreamModel({
         id: identifierMock.generateId(),
-        listenAddr: '0.0.0.0',
+        listenAddr: '114.25.11.1',
         listenPort: inputProxyWithPort.listenPort,
         proxyDownstream: [outputDownstreamProxyModel],
         runner: outputRunnerUpstreamModel,
