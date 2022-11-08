35a02444badabf9b11b919b7818760e9c431fcc5
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-11-08 11:45:26 +0330

    Add TDD unit test for ProxyService.create

--- src/core/service/proxy.service.spec.ts
+++ src/core/service/proxy.service.spec.ts
@@ -1,7 +1,6 @@
 import {Test, TestingModule} from '@nestjs/testing';
 import {ProxyService} from './proxy.service';
 import {mock, MockProxy} from 'jest-mock-extended';
-import {ProviderTokenEnum} from '@src-core/enum/provider-token.enum';
 import {IProxyRepositoryInterface} from '@src-core/interface/i-proxy-repository.interface';
 import {IIdentifier} from '@src-core/interface/i-identifier.interface';
 import {FilterModel} from '@src-core/model/filter.model';
@@ -15,20 +14,32 @@ import {
   RunnerStatusEnum,
 } from '@src-core/model/runner.model';
 import {MystIdentityModel} from '@src-core/model/myst-identity.model';
-import {VpnProviderModel} from '@src-core/model/vpn-provider.model';
+import {
+  VpnProviderIpTypeEnum,
+  VpnProviderModel,
+  VpnProviderName,
+  VpnServiceTypeEnum,
+} from '@src-core/model/vpn-provider.model';
+import {defaultModelFactory} from '@src-core/model/defaultModel';
+import {IProviderServiceInterface} from '@src-core/interface/i-provider-service.interface';
+import {NotFoundException} from '@src-core/exception/not-found.exception';
+import {ProviderIdentityNotConnectingException} from '@src-core/exception/provider-identity-not-connecting.exception';
 
 describe('ProxyService', () => {
   let service: ProxyService;
   let proxyRepository: MockProxy<IProxyRepositoryInterface>;
+  let providerService: MockProxy<IProviderServiceInterface>;
   let identifierMock: MockProxy<IIdentifier>;
 
   beforeEach(async () => {
     proxyRepository = mock<IProxyRepositoryInterface>();
+    providerService = mock<IProviderServiceInterface>();
 
     identifierMock = mock<IIdentifier>();
     identifierMock.generateId.mockReturnValue('00000000-0000-0000-0000-000000000000');
 
     const proxyRepositoryProvider = 'proxy-repository';
+    const providerServiceProvider = 'provider-service';
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
@@ -37,9 +48,14 @@ describe('ProxyService', () => {
           useValue: proxyRepository,
         },
         {
+          provide: providerServiceProvider,
+          useValue: providerService,
+        },
+        {
           provide: ProxyService,
-          inject: [proxyRepositoryProvider],
-          useFactory: (proxyRepository: IProxyRepositoryInterface) => new ProxyService(proxyRepository),
+          inject: [proxyRepositoryProvider, providerServiceProvider],
+          useFactory: (proxyRepository: IProxyRepositoryInterface, providerService: IProviderServiceInterface) =>
+            new ProxyService(proxyRepository, providerService),
         },
       ],
     }).compile();
@@ -60,7 +76,7 @@ describe('ProxyService', () => {
 
   describe(`Get all proxy`, () => {
     let inputFilter: FilterModel<ProxyUpstreamModel>;
-    let outputRunnerDownstreamModel: RunnerModel<MystIdentityModel>;
+    let outputRunnerDownstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
     let outputDownstreamProxyModel: ProxyDownstreamModel;
     let outputRunnerUpstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
     let outputProxyUpstreamModel: ProxyUpstreamModel;
@@ -68,27 +84,32 @@ describe('ProxyService', () => {
     beforeEach(() => {
       inputFilter = new FilterModel<ProxyUpstreamModel>();
 
-      outputRunnerDownstreamModel = new RunnerModel<MystIdentityModel>({
-        id: '11111111-1111-1111-1111-111111111111',
-        serial: 'myst-serial',
-        name: 'myst-name',
-        service: RunnerServiceEnum.MYST,
+      outputRunnerDownstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
+        id: '33333333-3333-3333-3333-333333333333',
+        serial: 'envoy-serial',
+        name: 'envoy-name',
+        service: RunnerServiceEnum.ENVOY,
         exec: RunnerExecEnum.DOCKER,
-        socketType: RunnerSocketTypeEnum.HTTP,
-        socketUri: '10.10.10.1',
-        socketPort: 4449,
+        socketType: RunnerSocketTypeEnum.TCP,
+        socketUri: '10.10.10.2',
+        socketPort: 10001,
         status: RunnerStatusEnum.RUNNING,
         insertDate: new Date(),
       });
-      outputRunnerDownstreamModel.label = {
-        $namespace: MystIdentityModel.name,
-        id: '22222222-2222-2222-2222-222222222222',
-        identity: 'identity1',
-      };
+      outputRunnerDownstreamModel.label = [
+        {
+          $namespace: MystIdentityModel.name,
+          id: '44444444-4444-4444-4444-444444444444',
+        },
+        {
+          $namespace: VpnProviderModel.name,
+          id: '55555555-5555-5555-5555-5555555555555',
+        },
+      ];
 
       outputDownstreamProxyModel = new ProxyDownstreamModel({
-        id: '33333333-3333-3333-3333-333333333333',
-        refId: '44444444-4444-4444-4444-444444444444',
+        id: '66666666-6666-6666-6666-666666666666',
+        refId: '77777777-7777-7777-7777-777777777777',
         ip: '25.14.65.1',
         mask: 32,
         type: ProxyTypeEnum.MYST,
@@ -97,13 +118,13 @@ describe('ProxyService', () => {
       });
 
       outputRunnerUpstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
-        id: '55555555-5555-5555-5555-555555555555',
+        id: '88888888-8888-8888-8888-888888888888',
         serial: 'socat-serial',
         name: 'socat-name',
         service: RunnerServiceEnum.SOCAT,
         exec: RunnerExecEnum.DOCKER,
         socketType: RunnerSocketTypeEnum.TCP,
-        socketUri: '10.10.10.2',
+        socketUri: '10.10.10.3',
         socketPort: 3128,
         status: RunnerStatusEnum.RUNNING,
         insertDate: new Date(),
@@ -111,11 +132,11 @@ describe('ProxyService', () => {
       outputRunnerUpstreamModel.label = [
         {
           $namespace: MystIdentityModel.name,
-          id: '22222222-2222-2222-2222-222222222222',
+          id: '44444444-4444-4444-4444-444444444444',
         },
         {
           $namespace: VpnProviderModel.name,
-          id: '66666666-6666-6666-6666-666666666666',
+          id: '55555555-5555-5555-5555-5555555555555',
         },
       ];
 
@@ -172,6 +193,209 @@ describe('ProxyService', () => {
       expect(result).toHaveLength(1);
       expect(result[0]).toEqual(outputProxyUpstreamModel);
       expect(total).toEqual(1);
+    });
+  });
+
+  describe(`Create new proxy`, () => {
+    let inputProxyWithPort: ProxyUpstreamModel;
+    let outputRunnerMyst: RunnerModel<MystIdentityModel>;
+    let outputVpnProviderIsNotRegistered: VpnProviderModel;
+    let outputVpnProviderIsRegister: VpnProviderModel;
+    let outputRunnerDownstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
+    let outputDownstreamProxyModel: ProxyDownstreamModel;
+    let outputRunnerUpstreamModel: RunnerModel<[MystIdentityModel, VpnProviderModel]>;
+    let outputProxyUpstreamModel: ProxyUpstreamModel;
+
+    beforeEach(() => {
+      inputProxyWithPort = defaultModelFactory(
+        ProxyUpstreamModel,
+        {
+          id: 'default-id',
+          listenAddr: 'listen-addr-default',
+          listenPort: 3128,
+          proxyDownstream: [
+            defaultModelFactory<ProxyDownstreamModel>(
+              ProxyDownstreamModel,
+              {
+                id: 'default-id',
+                refId: '44444444-4444-4444-4444-444444444444',
+                ip: 'default-ip',
+                mask: 32,
+                type: ProxyTypeEnum.MYST,
+                status: ProxyStatusEnum.OFFLINE,
+              },
+              ['id', 'ip', 'mask', 'status'],
+            ),
+          ],
+          insertDate: new Date(),
+        },
+        ['id', 'listenAddr', 'insertDate'],
+      );
+
+      outputRunnerMyst = new RunnerModel<MystIdentityModel>({
+        id: '11111111-1111-1111-1111-111111111111',
+        serial: 'myst-serial',
+        name: 'myst-name',
+        service: RunnerServiceEnum.MYST,
+        exec: RunnerExecEnum.DOCKER,
+        socketType: RunnerSocketTypeEnum.HTTP,
+        socketUri: '10.10.10.1',
+        socketPort: 4449,
+        status: RunnerStatusEnum.RUNNING,
+        insertDate: new Date(),
+      });
+      outputRunnerMyst.label = {
+        $namespace: MystIdentityModel.name,
+        id: '22222222-2222-2222-2222-222222222222',
+        identity: 'identity1',
+      };
+
+      outputVpnProviderIsNotRegistered = new VpnProviderModel({
+        id: identifierMock.generateId(),
+        serviceType: VpnServiceTypeEnum.WIREGUARD,
+        providerName: VpnProviderName.MYSTERIUM,
+        providerIdentity: 'provider-identity1',
+        providerIpType: VpnProviderIpTypeEnum.RESIDENTIAL,
+        country: 'GB',
+        isRegister: false,
+        insertDate: new Date(),
+      });
+      outputVpnProviderIsRegister = new VpnProviderModel({
+        id: identifierMock.generateId(),
+        userIdentity: outputRunnerMyst.label.identity,
+        serviceType: VpnServiceTypeEnum.WIREGUARD,
+        providerName: VpnProviderName.MYSTERIUM,
+        providerIdentity: 'provider-identity1',
+        providerIpType: VpnProviderIpTypeEnum.RESIDENTIAL,
+        ip: '25.14.65.1',
+        mask: 32,
+        country: 'GB',
+        runner: outputRunnerMyst,
+        isRegister: true,
+        insertDate: new Date(),
+      });
+
+      outputRunnerDownstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
+        id: '33333333-3333-3333-3333-333333333333',
+        serial: 'envoy-serial',
+        name: 'envoy-name',
+        service: RunnerServiceEnum.ENVOY,
+        exec: RunnerExecEnum.DOCKER,
+        socketType: RunnerSocketTypeEnum.TCP,
+        socketUri: '10.10.10.2',
+        socketPort: 10001,
+        status: RunnerStatusEnum.RUNNING,
+        insertDate: new Date(),
+      });
+      outputRunnerDownstreamModel.label = [
+        {
+          $namespace: MystIdentityModel.name,
+          id: '44444444-4444-4444-4444-444444444444',
+        },
+        {
+          $namespace: VpnProviderModel.name,
+          id: '55555555-5555-5555-5555-5555555555555',
+        },
+      ];
+
+      outputDownstreamProxyModel = new ProxyDownstreamModel({
+        id: '66666666-6666-6666-6666-666666666666',
+        refId: inputProxyWithPort.proxyDownstream[0].refId,
+        ip: outputVpnProviderIsRegister.ip,
+        mask: outputVpnProviderIsRegister.mask,
+        type: ProxyTypeEnum.MYST,
+        runner: outputRunnerDownstreamModel,
+        status: ProxyStatusEnum.ONLINE,
+      });
+
+      outputRunnerUpstreamModel = new RunnerModel<[MystIdentityModel, VpnProviderModel]>({
+        id: '88888888-8888-8888-8888-888888888888',
+        serial: 'socat-serial',
+        name: 'socat-name',
+        service: RunnerServiceEnum.SOCAT,
+        exec: RunnerExecEnum.DOCKER,
+        socketType: RunnerSocketTypeEnum.TCP,
+        socketUri: '10.10.10.3',
+        socketPort: 3128,
+        status: RunnerStatusEnum.RUNNING,
+        insertDate: new Date(),
+      });
+      outputRunnerUpstreamModel.label = [
+        {
+          $namespace: MystIdentityModel.name,
+          id: '44444444-4444-4444-4444-444444444444',
+        },
+        {
+          $namespace: VpnProviderModel.name,
+          id: '55555555-5555-5555-5555-5555555555555',
+        },
+      ];
+
+      outputProxyUpstreamModel = new ProxyUpstreamModel({
+        id: identifierMock.generateId(),
+        listenAddr: '0.0.0.0',
+        listenPort: inputProxyWithPort.listenPort,
+        proxyDownstream: [outputDownstreamProxyModel],
+        runner: outputRunnerUpstreamModel,
+        insertDate: new Date(),
+      });
+    });
+
+    it(`Should error create new proxy when get provider`, async () => {
+      providerService.getById.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.create(inputProxyWithPort);
+
+      expect(providerService.getById).toHaveBeenCalled();
+      expect(providerService.getById).toHaveBeenCalledWith(inputProxyWithPort.proxyDownstream[0].refId);
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should error create new proxy when get provider not found`, async () => {
+      providerService.getById.mockResolvedValue([null, null]);
+
+      const [error] = await service.create(inputProxyWithPort);
+
+      expect(providerService.getById).toHaveBeenCalled();
+      expect(providerService.getById).toHaveBeenCalledWith(inputProxyWithPort.proxyDownstream[0].refId);
+      expect(error).toBeInstanceOf(NotFoundException);
+    });
+
+    it(`Should error create new proxy when provider is not registered`, async () => {
+      providerService.getById.mockResolvedValue([null, outputVpnProviderIsNotRegistered]);
+
+      const [error] = await service.create(inputProxyWithPort);
+
+      expect(providerService.getById).toHaveBeenCalled();
+      expect(providerService.getById).toHaveBeenCalledWith(inputProxyWithPort.proxyDownstream[0].refId);
+      expect(error).toBeInstanceOf(ProviderIdentityNotConnectingException);
+    });
+
+    it(`Should error create new proxy`, async () => {
+      providerService.getById.mockResolvedValue([null, outputVpnProviderIsRegister]);
+      proxyRepository.create.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.create(inputProxyWithPort);
+
+      expect(providerService.getById).toHaveBeenCalled();
+      expect(providerService.getById).toHaveBeenCalledWith(inputProxyWithPort.proxyDownstream[0].refId);
+      expect(proxyRepository.create).toHaveBeenCalled();
+      expect(proxyRepository.create).toHaveBeenCalledWith(inputProxyWithPort);
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should successfully create new proxy`, async () => {
+      providerService.getById.mockResolvedValue([null, outputVpnProviderIsRegister]);
+      proxyRepository.create.mockResolvedValue([null, outputProxyUpstreamModel]);
+
+      const [error, result] = await service.create(inputProxyWithPort);
+
+      expect(providerService.getById).toHaveBeenCalled();
+      expect(providerService.getById).toHaveBeenCalledWith(inputProxyWithPort.proxyDownstream[0].refId);
+      expect(proxyRepository.create).toHaveBeenCalled();
+      expect(proxyRepository.create).toHaveBeenCalledWith(inputProxyWithPort);
+      expect(error).toBeNull();
+      expect(result).toEqual(outputProxyUpstreamModel);
     });
   });
 });
--- src/core/service/proxy.service.ts
+++ src/core/service/proxy.service.ts
@@ -4,18 +4,36 @@ import {FilterModel} from '@src-core/model/filter.model';
 import {ProxyUpstreamModel} from '@src-core/model/proxy.model';
 import {AsyncReturn} from '@src-core/utility';
 import {IProxyRepositoryInterface} from '@src-core/interface/i-proxy-repository.interface';
+import {IProviderServiceInterface} from '@src-core/interface/i-provider-service.interface';
+import {NotFoundException} from '@src-core/exception/not-found.exception';
+import {ProviderIdentityNotConnectingException} from '@src-core/exception/provider-identity-not-connecting.exception';
 
 @Injectable()
 export class ProxyService implements IProxyServiceInterface {
-  constructor(private readonly _proxyRepository: IProxyRepositoryInterface) {
+  constructor(
+    private readonly _proxyRepository: IProxyRepositoryInterface,
+    private readonly _providerService: IProviderServiceInterface,
+  ) {
   }
 
   async getAll(filter?: FilterModel<ProxyUpstreamModel>): Promise<AsyncReturn<Error, Array<ProxyUpstreamModel>>> {
     return this._proxyRepository.getAll(filter);
   }
 
-  create(model: ProxyUpstreamModel): Promise<AsyncReturn<Error, ProxyUpstreamModel>> {
-    return Promise.resolve(undefined);
+  async create(model: ProxyUpstreamModel): Promise<AsyncReturn<Error, ProxyUpstreamModel>> {
+    const providerId = model.proxyDownstream[0]?.refId;
+    const [vpnProviderError, vpnProviderData] = await this._providerService.getById(providerId);
+    if (vpnProviderError) {
+      return [vpnProviderError];
+    }
+    if (!vpnProviderData) {
+      return [new NotFoundException()];
+    }
+    if (!vpnProviderData.isRegister) {
+      return [new ProviderIdentityNotConnectingException()];
+    }
+
+    return this._proxyRepository.create(model);
   }
 
   remove(id: string): Promise<AsyncReturn<Error, null>> {
