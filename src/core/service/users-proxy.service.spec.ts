9af99257c8679bc9b2485403b494dc85be53cbe2
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-11-22 15:15:23 +0330

    Check user exist before get user's proxies list

--- src/core/service/users-proxy.service.spec.ts
+++ src/core/service/users-proxy.service.spec.ts
@@ -6,31 +6,40 @@ import {IUsersProxyRepositoryInterface} from '@src-core/interface/i-users-proxy-
 import {UsersProxyModel} from '@src-core/model/users-proxy.model';
 import {ProxyDownstreamModel, ProxyStatusEnum, ProxyTypeEnum} from '@src-core/model/proxy.model';
 import {UnknownException} from '@src-core/exception/unknown.exception';
+import {IUsersServiceInterface} from '@src-core/interface/i-users-service.interface';
+import {UsersModel} from '@src-core/model/users.model';
 
 describe('UsersProxyService', () => {
   let service: UsersProxyService;
+  let usersService: MockProxy<IUsersServiceInterface>;
   let usersProxyRepository: MockProxy<IUsersProxyRepositoryInterface>;
   let identifierMock: MockProxy<IIdentifier>;
 
   beforeEach(async () => {
+    usersService = mock<IUsersServiceInterface>();
     usersProxyRepository = mock<IUsersProxyRepositoryInterface>();
 
     identifierMock = mock<IIdentifier>();
     identifierMock.generateId.mockReturnValue('00000000-0000-0000-0000-000000000000');
 
+    const usersServiceProvider = 'users-service';
     const usersProxyRepositoryProvider = 'users-proxy-repository';
 
     const module: TestingModule = await Test.createTestingModule({
       providers: [
+        {
+          provide: usersServiceProvider,
+          useValue: usersService,
+        },
         {
           provide: usersProxyRepositoryProvider,
           useValue: usersProxyRepository,
         },
         {
           provide: UsersProxyService,
-          inject: [usersProxyRepositoryProvider],
-          useFactory: (usersProxyRepository: IUsersProxyRepositoryInterface) =>
-            new UsersProxyService(usersProxyRepository),
+          inject: [usersServiceProvider, usersProxyRepositoryProvider],
+          useFactory: (usersService: IUsersServiceInterface, usersProxyRepository: IUsersProxyRepositoryInterface) =>
+            new UsersProxyService(usersService, usersProxyRepository),
         },
       ],
     }).compile();
@@ -51,16 +60,25 @@ describe('UsersProxyService', () => {
 
   describe(`Get all user's proxies`, () => {
     let inputUserId: string;
+    let outputUserModel: UsersModel;
     let outputUsersProxyMode1: UsersProxyModel;
 
     beforeEach(() => {
       inputUserId = identifierMock.generateId();
 
+      outputUserModel = new UsersModel({
+        id: identifierMock.generateId(),
+        username: 'user1',
+        password: 'pass1',
+        isEnable: true,
+        insertDate: new Date(),
+      })
+
       outputUsersProxyMode1 = new UsersProxyModel({
         user: {
-          id: identifierMock.generateId(),
-          username: 'user1',
-          password: 'pass1',
+          id: outputUserModel.id,
+          username: outputUserModel.username,
+          password: outputUserModel.password,
         },
         id: identifierMock.generateId(),
         listenAddr: '26.110.20.6',
@@ -80,21 +98,37 @@ describe('UsersProxyService', () => {
       });
     });
 
+    it(`Should error get all user's proxies with user id when error get user by id`, async () => {
+      usersService.findOne.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.getByUserId(inputUserId);
+
+      expect(usersService.findOne).toHaveBeenCalled();
+      expect(usersService.findOne).toHaveBeenCalledWith(inputUserId);
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
     it(`Should error get all user's proxies with user id`, async () => {
+      usersService.findOne.mockResolvedValue([null, outputUserModel]);
       usersProxyRepository.getByUserId.mockResolvedValue([new UnknownException()]);
 
       const [error] = await service.getByUserId(inputUserId);
 
+      expect(usersService.findOne).toHaveBeenCalled();
+      expect(usersService.findOne).toHaveBeenCalledWith(inputUserId);
       expect(usersProxyRepository.getByUserId).toHaveBeenCalled();
       expect(usersProxyRepository.getByUserId).toHaveBeenCalledWith(inputUserId);
       expect(error).toBeInstanceOf(UnknownException);
     });
 
     it(`Should successfully get all user's proxies with user id`, async () => {
+      usersService.findOne.mockResolvedValue([null, outputUserModel]);
       usersProxyRepository.getByUserId.mockResolvedValue([null, [outputUsersProxyMode1], 1]);
 
       const [error, result, count] = await service.getByUserId(inputUserId);
 
+      expect(usersService.findOne).toHaveBeenCalled();
+      expect(usersService.findOne).toHaveBeenCalledWith(inputUserId);
       expect(usersProxyRepository.getByUserId).toHaveBeenCalled();
       expect(usersProxyRepository.getByUserId).toHaveBeenCalledWith(inputUserId);
       expect(error).toBeNull();
--- src/core/service/users-proxy.service.ts
+++ src/core/service/users-proxy.service.ts
@@ -3,13 +3,22 @@ import {IUsersProxyServiceInterface} from '@src-core/interface/i-users-proxy-ser
 import {AsyncReturn} from '@src-core/utility';
 import {UsersProxyModel} from '@src-core/model/users-proxy.model';
 import {IUsersProxyRepositoryInterface} from '@src-core/interface/i-users-proxy-repository.interface';
+import {IUsersServiceInterface} from '@src-core/interface/i-users-service.interface';
 
 @Injectable()
 export class UsersProxyService implements IUsersProxyServiceInterface {
-  constructor(private readonly _usersProxyRepository: IUsersProxyRepositoryInterface) {
+  constructor(
+    private readonly _usersService: IUsersServiceInterface,
+    private readonly _usersProxyRepository: IUsersProxyRepositoryInterface,
+  ) {
   }
 
   async getByUserId(userId: string): Promise<AsyncReturn<Error, Array<UsersProxyModel>>> {
+    const [userError] = await this._usersService.findOne(userId);
+    if (userError) {
+      return [userError];
+    }
+
     return this._usersProxyRepository.getByUserId(userId);
   }
 }
