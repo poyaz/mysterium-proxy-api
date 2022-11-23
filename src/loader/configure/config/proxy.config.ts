b8074011f196ce8f7dfe6db352c5e052e3d7930c
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-11-23 12:21:30 +0330

    Fix default path of acl

--- src/loader/configure/config/proxy.config.ts
+++ src/loader/configure/config/proxy.config.ts
@@ -6,6 +6,6 @@ export default registerAs('proxy', (): ProxyConfigInterface => {
   return {
     globalUpstreamAddress: process.env.PROXY_GLOBAL_UPSTREAM_ADDR,
     startUpstreamPort: Number(process.env.PROXY_START_UPSTREAM_PORT || 3128),
-    nginxAclFile: process.env.NGINX_ACL_FILE || resolve('storage', 'tmp', 'docker', 'nginx-conf', '001-proxy-acl.conf'),
+    nginxAclFile: process.env.NGINX_ACL_FILE || resolve('storage', 'docker', 'nginx-conf', '001-proxy-acl.conf'),
   };
 });
