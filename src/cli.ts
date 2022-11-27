d32f6673b7a326689c41221a0b1e6a592f7e4595
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-11-27 16:58:31 +0330

    Add DOCKER_REAL_PROJECT_PATH for run successfully cli

--- src/cli.ts
+++ src/cli.ts
@@ -1,3 +1,7 @@
+if (!process.env.DOCKER_REAL_PROJECT_PATH) {
+  process.env.DOCKER_REAL_PROJECT_PATH = __dirname;
+}
+
 import {CommandFactory} from 'nest-commander';
 import {CommandModule} from './command.module';
 
