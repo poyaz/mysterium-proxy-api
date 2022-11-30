6f050fc366402a084ed6ba46108570b5269cbc82
Pooya Azarpour <pooya_azarpour@yahoo.com>
2022-11-30 09:30:00 +0330

    Add TDD unit test for FavoritesService.getByUserId

---
+++ src/core/service/favorites.service.spec.ts
@@ -0,0 +1,232 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {FavoritesService} from './favorites.service';
+import {mock, MockProxy} from 'jest-mock-extended';
+import {IGenericRepositoryInterface} from '@src-core/interface/i-generic-repository.interface';
+import {FavoritesListTypeEnum, FavoritesModel} from '@src-core/model/favorites.model';
+import {IIdentifier} from '@src-core/interface/i-identifier.interface';
+import {FilterModel} from '@src-core/model/filter.model';
+import {IUsersServiceInterface} from '@src-core/interface/i-users-service.interface';
+import {UsersModel} from '@src-core/model/users.model';
+import {UnknownException} from '@src-core/exception/unknown.exception';
+import {defaultModelFactory} from '@src-core/model/defaultModel';
+import {UsersProxyModel} from '@src-core/model/users-proxy.model';
+import {ProxyDownstreamModel, ProxyStatusEnum, ProxyTypeEnum} from '@src-core/model/proxy.model';
+
+describe('FavoritesService', () => {
+  let service: FavoritesService;
+  let favoritesRepository: MockProxy<IGenericRepositoryInterface<FavoritesModel>>;
+  let usersService: MockProxy<IUsersServiceInterface>;
+  let identifierMock: MockProxy<IIdentifier>;
+
+  beforeEach(async () => {
+    favoritesRepository = mock<IGenericRepositoryInterface<FavoritesModel>>();
+    usersService = mock<IUsersServiceInterface>();
+
+    identifierMock = mock<IIdentifier>();
+    identifierMock.generateId.mockReturnValue('00000000-0000-0000-0000-000000000000');
+
+    const favoritesRepositoryProvider = 'favorites-repository';
+    const usersServiceProvider = 'users-service';
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: favoritesRepositoryProvider,
+          useValue: favoritesRepository,
+        },
+        {
+          provide: usersServiceProvider,
+          useValue: usersService,
+        },
+        {
+          provide: FavoritesService,
+          inject: [favoritesRepositoryProvider, usersServiceProvider],
+          useFactory: (
+            favoritesRepository: IGenericRepositoryInterface<FavoritesModel>,
+            usersService: IUsersServiceInterface,
+          ) =>
+            new FavoritesService(favoritesRepository, usersService),
+        },
+      ],
+    }).compile();
+
+    service = module.get<FavoritesService>(FavoritesService);
+
+    jest.useFakeTimers().setSystemTime(new Date('2020-01-01'));
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+    jest.resetAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+
+  describe(`Get favorites list by user id`, () => {
+    let inputUserId: string;
+    let inputFilter: FilterModel<FavoritesModel>;
+    let outputUserModel: UsersModel;
+    let outputUsersProxyMode: UsersProxyModel;
+    let outputFavoritesModel1: FavoritesModel;
+
+    beforeEach(() => {
+      inputUserId = identifierMock.generateId();
+
+      inputFilter = new FilterModel<FavoritesModel>();
+      inputFilter.addCondition({$opr: 'eq', kind: FavoritesListTypeEnum.FAVORITE});
+
+      outputUserModel = new UsersModel({
+        id: identifierMock.generateId(),
+        username: 'user1',
+        password: 'pass1',
+        insertDate: new Date(),
+      });
+
+      outputUsersProxyMode = new UsersProxyModel({
+        user: {
+          id: outputUserModel.id,
+          username: outputUserModel.username,
+          password: outputUserModel.password,
+        },
+        id: identifierMock.generateId(),
+        listenAddr: '26.110.20.6',
+        listenPort: 3128,
+        proxyDownstream: [
+          new ProxyDownstreamModel({
+            id: identifierMock.generateId(),
+            refId: identifierMock.generateId(),
+            ip: '65.23.45.12',
+            mask: 32,
+            country: 'GB',
+            type: ProxyTypeEnum.MYST,
+            status: ProxyStatusEnum.ONLINE,
+          }),
+        ],
+        insertDate: new Date(),
+      });
+
+      outputFavoritesModel1 = new FavoritesModel({
+        id: identifierMock.generateId(),
+        usersProxy: outputUsersProxyMode,
+        kind: FavoritesListTypeEnum.FAVORITE,
+        note: 'This is a note',
+        insertDate: new Date(),
+      });
+    });
+
+    it(`Should error get favorites list by user id when get user info (Without filter)`, async () => {
+      usersService.findOne.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.getByUserId(inputUserId);
+
+      expect(usersService.findOne).toHaveBeenCalled();
+      expect(usersService.findOne).toHaveBeenCalledWith(inputUserId);
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should error get favorites list by user id (Without filter)`, async () => {
+      usersService.findOne.mockResolvedValue([null, outputUserModel]);
+      favoritesRepository.getAll.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.getByUserId(inputUserId);
+
+      expect(usersService.findOne).toHaveBeenCalled();
+      expect(usersService.findOne).toHaveBeenCalledWith(inputUserId);
+      expect(favoritesRepository.getAll).toHaveBeenCalled();
+      expect((<FilterModel<FavoritesModel>>favoritesRepository.getAll.mock.calls[0][0]).getConditionList()).toHaveLength(1);
+      expect((<FilterModel<FavoritesModel>>favoritesRepository.getAll.mock.calls[0][0]).getCondition('usersProxy')).toEqual({
+        $opr: 'eq',
+        usersProxy: defaultModelFactory<UsersProxyModel>(
+          UsersProxyModel,
+          {
+            id: 'default-id',
+            listenAddr: 'default-listen-addr',
+            listenPort: 0,
+            user: {
+              id: inputUserId,
+              username: 'default-username',
+              password: 'default-password',
+            },
+            proxyDownstream: [],
+            insertDate: new Date(),
+          },
+          ['id', 'listenAddr', 'listenPort', 'proxyDownstream', 'insertDate'],
+        ),
+      });
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should error get favorites list by user id (With filter)`, async () => {
+      usersService.findOne.mockResolvedValue([null, outputUserModel]);
+      favoritesRepository.getAll.mockResolvedValue([new UnknownException()]);
+
+      const [error] = await service.getByUserId(inputUserId, inputFilter);
+
+      expect(usersService.findOne).toHaveBeenCalled();
+      expect(usersService.findOne).toHaveBeenCalledWith(inputUserId);
+      expect(favoritesRepository.getAll).toHaveBeenCalled();
+      expect((<FilterModel<FavoritesModel>>favoritesRepository.getAll.mock.calls[0][0]).getConditionList()).toHaveLength(2);
+      expect((<FilterModel<FavoritesModel>>favoritesRepository.getAll.mock.calls[0][0]).getCondition('usersProxy')).toEqual({
+        $opr: 'eq',
+        usersProxy: defaultModelFactory<UsersProxyModel>(
+          UsersProxyModel,
+          {
+            id: 'default-id',
+            listenAddr: 'default-listen-addr',
+            listenPort: 0,
+            user: {
+              id: inputUserId,
+              username: 'default-username',
+              password: 'default-password',
+            },
+            proxyDownstream: [],
+            insertDate: new Date(),
+          },
+          ['id', 'listenAddr', 'listenPort', 'proxyDownstream', 'insertDate'],
+        ),
+      });
+      expect((<FilterModel<FavoritesModel>>favoritesRepository.getAll.mock.calls[0][0]).getCondition('kind')).toEqual({
+        $opr: 'eq',
+        kind: FavoritesListTypeEnum.FAVORITE,
+      });
+      expect(error).toBeInstanceOf(UnknownException);
+    });
+
+    it(`Should successfully get favorites list by user id (Without filter)`, async () => {
+      usersService.findOne.mockResolvedValue([null, outputUserModel]);
+      favoritesRepository.getAll.mockResolvedValue([null, [outputFavoritesModel1], 1]);
+
+      const [error, result, total] = await service.getByUserId(inputUserId);
+
+      expect(usersService.findOne).toHaveBeenCalled();
+      expect(usersService.findOne).toHaveBeenCalledWith(inputUserId);
+      expect(favoritesRepository.getAll).toHaveBeenCalled();
+      expect((<FilterModel<FavoritesModel>>favoritesRepository.getAll.mock.calls[0][0]).getConditionList()).toHaveLength(1);
+      expect((<FilterModel<FavoritesModel>>favoritesRepository.getAll.mock.calls[0][0]).getCondition('usersProxy')).toEqual({
+        $opr: 'eq',
+        usersProxy: defaultModelFactory<UsersProxyModel>(
+          UsersProxyModel,
+          {
+            id: 'default-id',
+            listenAddr: 'default-listen-addr',
+            listenPort: 0,
+            user: {
+              id: inputUserId,
+              username: 'default-username',
+              password: 'default-password',
+            },
+            proxyDownstream: [],
+            insertDate: new Date(),
+          },
+          ['id', 'listenAddr', 'listenPort', 'proxyDownstream', 'insertDate'],
+        ),
+      });
+      expect(error).toBeNull();
+      expect(result).toHaveLength(1);
+      expect(result[0]).toEqual(outputFavoritesModel1);
+      expect(total).toEqual(1);
+    });
+  });
+});
---
+++ src/core/service/favorites.service.ts
@@ -0,0 +1,65 @@
+import {Injectable} from '@nestjs/common';
+import {IFavoritesServiceInterface} from '@src-core/interface/i-favorites-service.interface';
+import {UpdateModel} from '@src-core/model/update.model';
+import {FavoritesListTypeEnum, FavoritesModel} from '@src-core/model/favorites.model';
+import {AsyncReturn} from '@src-core/utility';
+import {FilterModel} from '@src-core/model/filter.model';
+import {IGenericRepositoryInterface} from '@src-core/interface/i-generic-repository.interface';
+import {IUsersServiceInterface} from '@src-core/interface/i-users-service.interface';
+import {defaultModelFactory} from '@src-core/model/defaultModel';
+import {UsersProxyModel} from '@src-core/model/users-proxy.model';
+
+@Injectable()
+export class FavoritesService implements IFavoritesServiceInterface {
+  constructor(
+    private readonly _favoritesRepository: IGenericRepositoryInterface<FavoritesModel>,
+    private readonly _usersService: IUsersServiceInterface,
+  ) {
+  }
+
+  async getByUserId(userId: string, filter?: FilterModel<FavoritesModel>): Promise<AsyncReturn<Error, Array<FavoritesModel>>> {
+    const [userError] = await this._usersService.findOne(userId);
+    if (userError) {
+      return [userError];
+    }
+
+    const filterModel = !filter ? new FilterModel<FavoritesModel>() : filter;
+    filterModel.addCondition({
+      $opr: 'eq',
+      usersProxy: defaultModelFactory<UsersProxyModel>(
+        UsersProxyModel,
+        {
+          id: 'default-id',
+          listenAddr: 'default-listen-addr',
+          listenPort: 0,
+          user: {
+            id: userId,
+            username: 'default-username',
+            password: 'default-password',
+          },
+          proxyDownstream: [],
+          insertDate: new Date(),
+        },
+        ['id', 'listenAddr', 'listenPort', 'proxyDownstream', 'insertDate'],
+      ),
+    });
+
+    return this._favoritesRepository.getAll<FilterModel<FavoritesModel>>(filterModel);
+  }
+
+  createBulk(models: Array<FavoritesModel>): Promise<AsyncReturn<Error, Array<FavoritesModel>>> {
+    return Promise.resolve(undefined);
+  }
+
+  update(model: UpdateModel<FavoritesModel>): Promise<AsyncReturn<Error, null>> {
+    return Promise.resolve(undefined);
+  }
+
+  updateBulkKind(kind: FavoritesListTypeEnum, proxiesListId: Array<string>): Promise<AsyncReturn<Error, null>> {
+    return Promise.resolve(undefined);
+  }
+
+  removeBulk(favoritesListId: Array<string>): Promise<AsyncReturn<Error, null>> {
+    return Promise.resolve(undefined);
+  }
+}
