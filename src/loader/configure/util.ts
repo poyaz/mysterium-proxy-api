f88aad3c74553f189b08adbb82da58e533ec1026
Pooya Azarpour <pooya_azarpour@yahoo.com>
2023-01-09 11:11:01 +0330

    Fix undifined string

--- src/loader/configure/util.ts
+++ src/loader/configure/util.ts
@@ -1,4 +1,7 @@
-
 export function convertStringToBoolean(value: string): boolean {
+  if (!value) {
+    return false;
+  }
+
   return ['true', 't', 'yes', 'y', '1'].indexOf(value.toLocaleLowerCase()) > -1;
 }
